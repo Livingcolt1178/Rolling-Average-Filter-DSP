5b18cd3e208a833c70dfe295c6d1bfbb2ea7dce5
Nicholas <nrbramhall1@gmail.com>
2026-03-24 17:18:45 -0400

    finished rollingAverageFilter.sv and its tesbench

--- Rolling Average Filter DSP.srcs/sources_1/new/rollingAverageFilter.sv
+++ Rolling Average Filter DSP.srcs/sources_1/new/rollingAverageFilter.sv
@@ -8,16 +8,38 @@
     The processing will be a rolling average filter that takes in 8 samples and outputs the average of those samples. 
     The input data will be 8 bits and the output data will be 12 bits.
     */
-module DSP (
+module rollingAverageFilter (
     input logic clk,
     input logic rst_n,
 
-    input logic [7:0] DSP_Din,      //The Data coming in from the ADC
+    input logic [7:0] filter_Din,      //The Data coming in from the ADC
 
+    output logic [11:0] filter_Dout,   //The Data leaving to the DAC
+    output logic filter_ready          //Signal to let the ADC know its ready for more data.
+);
+    logic [7:0] samples [3:0];
+    logic [9:0] sum;
+    logic [7:0] avg;
 
-    output logic [11:0] DSP_Dout,   //The Data leaving to the DAC
-    output logic DSP_ready          //Signal to let the ADC know its ready for more data.
-    );
+    always_ff @(posedge clk) begin
+        if(!rst_n) begin
+            for(int i = 0; i < 4; i++) begin
+                samples[i] <= 8'b0; //reset the samples to 0 on reset
+            end
+        end else begin
+            samples[3] <= samples[2]; //shift in the new data, the oldest data will be overwritten.
+            samples[2] <= samples[1]; //shift in the new data, the oldest data will be overwritten.
+            samples[1] <= samples[0]; //shift in the new data, the oldest data will be overwritten.
+            samples[0] <= filter_Din; //shift in the new data, the oldest data will be overwritten.
+        end
+    end
+    //========================================================
+    // Rolling Average Calculation
+    //========================================================
+    assign sum = samples[0] + samples[1] + samples[2] + samples[3];
+    assign avg = sum >> 2; //divide the sum by 4 to get the average, this is done by shifting right by 2 bits.
+    assign filter_Dout = {4'b0, avg}; //Output the average as a 12-bit value
 
-    
+    assign filter_ready = 1'b1; //Signal that the DSP is ready for more data
+
 endmodule
